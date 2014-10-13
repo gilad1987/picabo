ace25702dd944eef78fd07ec7da9c60c0aae5a91
gilad1987 <gilad19872@gmail.com>
2014-10-13 09:19:35 +0300

    Update Base_Controller

--- api/App/Controller/Site/Uploads.php
+++ api/App/Controller/Site/Uploads.php
@@ -14,7 +14,7 @@ class App_Controller_Site_Uploads extends App_Controller_Site_Base
         $response = new stdClass();
         $response->url = BASE_URL.$image_token;
         $response->success = true;
-        $this->_view->setDisableView(false);
+        $this->_view->setDisableView(true);
         $this->_view->response = $response;
 
         $this->writeDump();
